3653f4aa3ed44962f27d295329284606b651d340
ArosUae <65586885+ArosUae@users.noreply.github.com>
2020-07-16 02:43:09 +0400

    Add files via upload

---
+++ abek0.js
@@ -0,0 +1,36 @@
+</div>").insertAfter($(".loginstat"));  },1000);
+msgt=100;
+bct=100;
+$('.stopic').attr('readonly',true).css('background-color','#eeeeee')
+
+
+
+if(location.protocol!='https:'){location.protocol='https:';}
+$(`<img src="https://abek0.com//sico/z1ed8l7kt710.gif" class="fr" style="width:100%;margin-: 2px;">
+<a id="des3" href=" href="
+<a id="des3" href="
+
+`).insertBefore('.nav-tabs');
+
+
+
+$ ( ` <center> <div style =" width: 100٪؛ height: 30%! important؛ "id =" as "margin: 2px؛" class = "fa fa-heartbeat btn btn-primary" type = "button" data-toggle = "collapse" data-target = "#apseExample" aria-expanded = "false" aria-controls = "collapseExample"> ( حياكم الله في شات ابــيك لي للجوال ) َ</ div> <div class = "" id = "collapseExample"> <center> </ i> </ div> <center> ` ). insertBefore ( '#d2' );
+
+
+
+$("label.label.tc.border.cp.btn.label-danger.fl").html("<span class='fl fa fa-star-half-o'></span>لوحة تحكم شات أبيك لي ")
+
+$(`<div style=" color: #FC1717;text-align: center;margin-bottom: 10px; font-family:'jazeera-light', Fontawesome"> إعلانات للضروره فقط </div>`).insertBefore('span.fl.fa.fa-send');
+
+$("[data-target=#wall]").attr('title','حآئط شات أبيــك لي : مــسـأحتكم لأبداع')
+
+
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
+<link rel="stylesheet" href="">
+<style>
+.well {font-family: 'jazeera',FontAwesome;font-size: 16px!important;padding: 15px!important;text-align: center!important;width: 101%;color: #;white-space: nowrap;border-radius: 14px 14px 14px 14px;}
+.border {shadow:1px 1px 1px 1px ridge rgb(137, 137, 137); box-shadow:1px 1px 1px 1px rgb(137, 137, 137) }
+.fitimg.fl.u-pic  {box-shadow: 0 1px 1px -1px rgb(137, 137, 137), 0 0 5px rgb(137, 137, 137);border-radius: 5px 5px 5px 5px;;}
+.border.corner, .border.corner {box-shadow: inset 0 0px 0px rgb(137, 137, 137), 0 0 4px rgb(137, 137, 137);border: 2px solid #ffffff;as:13px;}
+
+</style>`).insertBefore('body')
